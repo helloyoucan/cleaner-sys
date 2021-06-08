77cc9bd38250bc8da012a024f1129b5dc251fb91
wucanlong <wucanlong@bell.ai>
2021-06-08 15:01:48 +0800

    update#

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header><slot name="header"></slot></el-header>
+    <el-container>
+      <el-aside width="200px"><slot name="aside"></slot></el-aside>
+      <el-main><slot name="main"></slot></el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
